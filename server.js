7b05dc6def01a5799c9d8c1a41ede07750371d12
Joshua Sands <josh.sands@mail.com>
2021-07-25 12:24:00 -0500

    added new note function

--- server.js
+++ server.js
@@ -19,7 +19,35 @@ app.get("/notes", (req, res) => {
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
-  });  
+  });
+
+  app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+function newNote(content, allNotesArray) {
+    const newNote = content;
+    if (!Array.isArray(allNotesArray))
+        allNotesArray = [];
+    
+    if (allNotesArray.length === 0)
+        allNotesArray.push(0);
+
+    content.id = allNotesArray[0];
+    allNotesArray[0]++;
+
+    allNotesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(allNotesArray, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = newNote(req.content, allNotes);
+    res.json(newNote);
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
