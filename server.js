02363886de01090e776837f7e9fc6ae0c0cd42f1
Joshua Sands <josh.sands@mail.com>
2021-07-25 12:49:34 -0500

    added delete function

--- server.js
+++ server.js
@@ -1,54 +1,74 @@
-const fs = require("fs");
-const path = require("path");
-
-const express = require("express");
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+const allNotes = require('./Develop/db/db.json');
+
 app.use(express.json());
-app.use(express.static(path.join(__dirname + './Develop/public')));
+app.use(express.static('Develop/public'));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
-});
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
-  });
-
-  app.get('/api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
 
-function newNote(content, allNotesArray) {
-    const newNote = content;
+// return index.html when user opens root/homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+// return the notes.html file when user clicks on get started button path to root/notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+function createnote(body, allNotesArray) {
+    const newNote = body;
     if (!Array.isArray(allNotesArray))
         allNotesArray = [];
     
     if (allNotesArray.length === 0)
         allNotesArray.push(0);
 
-    content.id = allNotesArray[0];
+    body.id = allNotesArray[0];
     allNotesArray[0]++;
 
     allNotesArray.push(newNote);
     fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
+        path.join(__dirname, './Develop/db/db.json'),
         JSON.stringify(allNotesArray, null, 2)
     );
     return newNote;
 }
 
 app.post('/api/notes', (req, res) => {
-    const newNote = newNote(req.content, allNotes);
+    const newNote = createnote(req.body, allNotes);
     res.json(newNote);
 });
 
+function deleteNote(id, allNotesArray) {
+    for (let i = 0; i < allNotesArray.length; i++) {
+        let note = allNotesArray[i];
+
+        if (note.id == id) {
+            allNotesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './Develop/db/db.json'),
+                JSON.stringify(allNotesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 });
